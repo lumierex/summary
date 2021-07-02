a83065938db843b1ca20848cbaf508da08d9ed25
root <root@JDDBJ1PF15YV94.360buyAD.local>
2021-07-02 15:10:01 +0800

    fix: template

--- .umirc.ts
+++ .umirc.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   title: 'notes',
   // favicon: '',
   // logo: '',
+  publicPath: process.env.NODE_ENV === 'production' ? '/go-notes/' : '/',
   outputPath: 'docs-dist',
   mode: 'site',
   // more config: https://d.umijs.org/config
